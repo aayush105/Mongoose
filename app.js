42792e1de29e7237392b99959b47b698396fb411
aayush105 <sthaaayush105@gmail.com>
2023-06-01 20:42:26 +0545

    complete database created

--- app.js
+++ app.js
@@ -5,24 +5,49 @@ const mongoose = require("mongoose");
 // Connect URL and database
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
-// creating schema
+// creating schema and validating the data
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    score: Number,
+    name: {
+        type: String,
+        required:[true, "Please check your data entry, no name specified!"]
+    },
+    score: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
 // creating the model
 const Fruit = mongoose.model("Fruit", fruitSchema); // here Fruits(singular) will be converted into fruits(plural)
 
-// inserting the data into DB
-const fruit = new Fruit ({
-    name: "Appple",
-    score: 8,
-    review: "Great fruit"
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    favouriteFruit : fruitSchema   // embedding the fruit schema
 });
 
+const Person = mongoose.model("Person", personSchema); // here person(singular) will be converted to the people(plural)
+
+// inserting the data into DB of fruit schema
+const fruit = new Fruit ({
+    score: 8,
+    review: "this fruit is great"
+});
+
+// to save the data into DB
+// fruit.save();
+
+
 // inserting many data into DB
+const pineapple= new Fruit({
+    name: "Pineapple",
+    score:9,
+    review: "Great fruit"
+}); 
+// pineapple.save();
 
 const orange = new Fruit({
     name: "Orange",
@@ -42,23 +67,93 @@ const melon = new Fruit({
     review: "Awesome taste"
 });
 
-Fruit.insertMany([orange, mango, melon]);
+// Fruit.insertMany([orange, mango, melon]); 
 
 
 
-// to save the data into DB
-//fruit.save();
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
-
-const Person = mongoose.model("Person", personSchema); // here person(singular) will be converted to the people(plural)
+// inserting the data into DB of person schema using embedded documents
 
 const person = new Person({
-    name: "Aayush",
-    age: 23
+    name: "Aayush shrestha",
+    age: 23,
+    favouriteFruit: pineapple //embedding the data 
 });
 
-//person.save();
+// person.save();
+
+/*
+//updateing the data to add fav fruit
+async function updateData() {
+    try {
+      await Person.updateOne({_id: "64788d21cbafb6b378cf4b66"}, {favouriteFruit: mango});
+      console.log("Successfully updated");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  updateData();
+*/
+
+/*
+// reading the inserted data from the database
+//new version of mongoose
+async function queryData() {
+    try {
+      const result = await Fruit.find();
+      console.log('Query result:', result);
+    } catch (error) {
+      console.error('Error querying data:', error);
+    }
+  }
+  queryData();
+ */
+
+// reading the name of all the data
+async function queryData() {
+    try {
+        const result = await Fruit.find(); // o/p : fruits
+        // loop through each item
+        result.forEach(function(fruit){ 
+        console.log(fruit.name);
+        mongoose.connection.close(); // to close the connection to DB
+      });
+    } catch (error) {
+      console.error('Error querying data:', error);
+    }
+  }
+  queryData();
+
+/*
+// update the data into DB
+  async function updateData() {
+    try {
+      await Fruit.updateOne({_id: "647899b05f602c052bd8c2c2"}, {name: "Peach"});
+      console.log("Successfully updated");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  updateData();
+
+  // delete the data from DB
+  async function deleteData(){
+    try{
+        await Fruit.deleteOne({ name: "Peach" }); // use "deleteMany" to delete many datas
+        console.log("Successfully deleted");
+    } catch(err) {
+        console.error(err);
+    }
+  }
+  deleteData();
+  
+*/
+
+
+// new version of mongoose doesn't support call back function, use this if older version
+// Fruit.find(function(err, fruits){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(fruits);
+//     }
+// });
